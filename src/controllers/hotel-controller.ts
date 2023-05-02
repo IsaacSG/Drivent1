cdd1bff7075b2f9d98fcd852c45abaffb8a34bd4
Isaac Graciano <isaacgraciano123@gmail.com>
2023-05-02 00:39:33 -0300

    Booking

--- src/controllers/hotel-controller.ts
+++ src/controllers/hotel-controller.ts
@@ -1,37 +1,28 @@
-import { Response } from 'express';
 import httpStatus from 'http-status';
+import { NextFunction, Response } from 'express';
 import { AuthenticatedRequest } from '@/middlewares';
-import hotelService from '@/services/hotel-service';
+import hotelsService from '@/services/hotel-service';
 
-export async function getHotels(req: AuthenticatedRequest, res: Response) {
+export async function getHotels(req: AuthenticatedRequest, res: Response, next: NextFunction) {
   const { userId } = req;
 
   try {
-    const hotels = await hotelService.getHotels(Number(userId));
+    const hotels = await hotelsService.getHotels(userId);
     return res.status(httpStatus.OK).send(hotels);
   } catch (error) {
-    if (error.name === 'NotFoundError') {
-      return res.sendStatus(httpStatus.NOT_FOUND);
-    }
-    return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
+    next(error);
   }
 }
 
-export async function getHotelsWithRooms(req: AuthenticatedRequest, res: Response) {
+export async function getHotelsWithRooms(req: AuthenticatedRequest, res: Response, next: NextFunction) {
   const { userId } = req;
   const { hotelId } = req.params;
 
   try {
-    const hotels = await hotelService.getHotelsWithRooms(Number(userId), Number(hotelId));
+    const hotels = await hotelsService.getHotelsWithRooms(userId, Number(hotelId));
 
     return res.status(httpStatus.OK).send(hotels);
   } catch (error) {
-    if (error.name === 'NotFoundError') {
-      return res.sendStatus(httpStatus.NOT_FOUND);
-    }
-    if (error.name === 'CannotListHotelsError') {
-      return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
-    }
-    return res.sendStatus(httpStatus.BAD_REQUEST);
+    next(error);
   }
 }
--- src/services/hotel-service/index.ts
+++ src/services/hotel-service/index.ts
@@ -1,17 +1,15 @@
 import hotelRepository from '@/repositories/hotel-repository';
 import enrollmentRepository from '@/repositories/enrollment-repository';
-import ticketRepository from '@/repositories/tickets-repository';
 import { notFoundError } from '@/errors';
+import ticketsRepository from '@/repositories/tickets-repository';
 import { cannotListHotelsError } from '@/errors/cannot-list-hotels';
 
 async function listHotels(userId: number) {
-  //Tem enrollment?
   const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
   if (!enrollment) {
     throw notFoundError();
   }
-  //Tem ticket pago isOnline false e includesHotel true
-  const ticket = await ticketRepository.findTicketByEnrollmentId(enrollment.id);
+  const ticket = await ticketsRepository.findTicketByEnrollmentId(enrollment.id);
 
   if (!ticket || ticket.status === 'RESERVED' || ticket.TicketType.isRemote || !ticket.TicketType.includesHotel) {
     throw cannotListHotelsError();
@@ -22,22 +20,24 @@ async function getHotels(userId: number) {
   await listHotels(userId);
 
   const hotels = await hotelRepository.findHotels();
+  if (!hotels || hotels.length === 0) {
+    throw notFoundError();
+  }
   return hotels;
 }
 
 async function getHotelsWithRooms(userId: number, hotelId: number) {
   await listHotels(userId);
+
   const hotel = await hotelRepository.findRoomsByHotelId(hotelId);
 
-  if (!hotel) {
+  if (!hotel || hotel.Rooms.length === 0) {
     throw notFoundError();
   }
   return hotel;
 }
 
-const hotelService = {
+export default {
   getHotels,
   getHotelsWithRooms,
 };
-
-export default hotelService;
